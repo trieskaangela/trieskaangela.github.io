3079d4c53d50d5e901d9b2cb24dfd922e6583304
trieskaangela <52693112+trieskaangela@users.noreply.github.com>
2019-07-09 23:09:49 +0700

    Add files via upload

--- js-ok/function.js
+++ js-ok/function.js
@@ -1,13 +1,8 @@
+/* Start JS */
 /* Demo purposes only */
-var snippet = [].slice.call(document.querySelectorAll('.hover'));
-if (snippet.length) {
-  snippet.forEach(function (snippet) {
-    snippet.addEventListener('mouseout', function (event) {
-      if (event.target.parentNode.tagName === 'figure') {
-        event.target.parentNode.classList.remove('hover')
-      } else {
-        event.target.parentNode.classList.remove('hover')
-      }
-    });
-  });
-}
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+/*End JS*/
